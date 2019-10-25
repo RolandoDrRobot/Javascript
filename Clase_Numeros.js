47d480c340904e10480898499928b939a2b0a16a
RolandoDrRobot <rolando.arguello.email@gmail.com>
2019-10-24 22:21:36 -0600

    Primer commit / Creacion de Clase_Numeros.js

---
+++ Clase_Numeros.js
@@ -0,0 +1,17 @@
+var peso = 10
+
+var sandwich = 3
+
+peso += sandwich
+
+var jugarFutbol = 3
+
+peso += jugarFutbol
+
+var precioDelVino = 200.3
+
+var total = Math.round(precioDelVino * 100 * 3) / 100
+
+var totalStr = total.toFixed(3)
+
+var total2 = parseFloat(totalStr)
