8ba4f415f4e61d2230faf64156e2ade4264baf87
RolandoDrRobot <rolando.arguello.email@gmail.com>
2019-10-28 22:18:03 -0600

    Creamos archivo Control.js / Se crea practica de IF()

---
+++ Conditional.js
@@ -0,0 +1,21 @@
+var sacha = {
+	nombre: 'Sacha',
+	apellido: 'Lifszyc',
+	edad: 28,
+	ingeniero: true,
+	cocinero: false,
+	cantante: false,
+	dj: false,
+	drone: true
+}
+
+function imprimirProfesiones(persona) {
+	console.log ('${persona.nombre} es:')
+
+	if (persona.ingeniero) {
+		console.log('Ingeniero')
+	}	
+	else{
+		console.log('No es Ingeniero')
+	}	
+}
