1c4b44688ce578a6c9e9641e73510153e24fcd69
RolandoDrRobot <rolando.arguello.email@gmail.com>
2020-02-06 10:49:40 -0600

    Added omre examples for spread operator

--- ecma6+LTV-presentation/es6/spreadOperator.js
+++ ecma6+LTV-presentation/es6/spreadOperator.js
@@ -3,13 +3,26 @@
 // Copying an array
 let fruits = ['Apple','Orange','Banana'];
 let newFruitArray = [...fruits];
-console.log(copiedList); // ['Apple','Orange','Banana']
+console.log(newFruitArray); // ['Apple','Orange','Banana']
 
 // Concatenating arrays
 let arr1 = ['A', 'B', 'C'];
 let arr2 = ['X', 'Y', 'Z'];
 let result = [...arr1, ...arr2];
 console.log(result); // ['A', 'B', 'C', 'X', 'Y', 'Z']
+
+// Spread syntax for object literals
+var obj1 = { id: 101, name: 'Jhon Doe' };
+var obj2 = { age: 25, country: 'USA'};
+const employee = { ...obj1, ...obj2 };
+console.log(employee); //{ "id": 101, "name": "Jhon Doe", "age": 25, "country": "USA" }
+// What happens if we try to spread an onject an array
+/*
+var obj1 = { id: 101, name: 'Jhon Doe' };
+var obj2 = [ ['age', 25], ['country', 'USA'] ];
+const employee = { ...obj1, ...obj2 };
+console.log(employee);
+*/
 
 // Spreading elements on function calls
 let fruits = ['Apple','Orange','Banana'];
@@ -20,8 +33,11 @@ var getFruits = (f1, f2, f3) => {
 
 getFruits(...fruits); // Fruits: Apple, Orange and Banana
 
-// Spread syntax for object literals
-var obj1 = { id: 101, name: 'Jhon Doe' };
-var obj2 = { age: 25, country: 'USA'};
-const employee = { ...obj1, ...obj2 };
-console.log(employee); //{ "id": 101, "name": "Jhon Doe", "age": 25, "country": "USA" }
+
+
+// Separate Charts, ONLY strings, no numbers
+var alphabets = ["A", ..."123124", "E"];
+console.log(alphabets);
+// What is the ouput expected?
+var max = Math.max(..."43210");
+console.log(max);
